23a42249b23db39cf6a66c24a067687c51ff1659
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2022-09-02 14:31:01 -0300

    boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState("");
+
+  const handleSubmit = async (e) => {};
+
+  const handleLogout = async () => {};
+
+  if (user) {
+    return (
+      <div className="App">
+        <p>{user} is loggged in</p>
+        <button type="button" onClick={handleLogout}>
+          Logout
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username: </label>
+        <input
+          type="text"
+          value={username}
+          placeholder="enter a username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
